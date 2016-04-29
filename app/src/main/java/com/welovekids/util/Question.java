b0649452fb42d17651fd09a801c1e114352aae5a
Chris Rabe <chris.rabe14@gmail.com>
2016-04-30 11:22:37 +1200

    Improved question abstract class

--- app/src/main/java/com/welovekids/util/Question.java
+++ app/src/main/java/com/welovekids/util/Question.java
@@ -1,7 +1,15 @@
 package com.welovekids.util;
 
-/**
- * Created by chris on 30/04/2016.
- */
-public interface Question {
+
+public interface Question{
+    /** retrieves the question as a string */
+    public String getQuestion();
+    /** retrieves the answer as a string */
+    public String getAnswer();
+    /** sets the question */
+    public void setQuestion(String question);
+    /** sets the answer */
+    public void setAnswer(String answer);
+    /** prints the question -> answer pair */
+    public void printQuestion();
 }
