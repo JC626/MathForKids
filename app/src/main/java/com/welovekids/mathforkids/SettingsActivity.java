2d810ab50b9327813a9cdda25c946913d655ea38
MeganLiang <meganliang15@hotmail.com>
2016-04-30 07:12:54 +1200

    "static booleans for setting buttons"

--- app/src/main/java/com/welovekids/mathforkids/SettingsActivity.java
+++ app/src/main/java/com/welovekids/mathforkids/SettingsActivity.java
@@ -11,9 +11,9 @@ import com.welovekids.util.Controller;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private static boolean basicB;
-    private static boolean intermediateB;
-    private static boolean advancedB;
+    private static boolean basicB=false;
+    private static boolean intermediateB=false;
+    private static boolean advancedB=false;
     private Switch basic;
     private Switch intermediate;
     private Switch advanced;
@@ -28,9 +28,9 @@ public class SettingsActivity extends AppCompatActivity {
         intermediate = (Switch) findViewById(R.id.intermediate);
         advanced = (Switch) findViewById(R.id.advanced);
         //set the switch to OFF
-        basic.setChecked(false);
-        intermediate.setChecked(false);
-        advanced.setChecked(false);
+        basic.setChecked(basicB);
+        intermediate.setChecked(intermediateB);
+        advanced.setChecked(advancedB);
         //attach a listener to check for changes in state
         basic.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
@@ -39,9 +39,14 @@ public class SettingsActivity extends AppCompatActivity {
                                          boolean isChecked) {
 
                 if (isChecked) {
-                    basic.setChecked(true);
-                    intermediate.setChecked(false);
-                    advanced.setChecked(false);
+                    basicB=true;
+                    if(basicB==true) {
+                        intermediateB=false;
+                        advancedB=false;
+                        basic.setChecked(basicB);
+                        intermediate.setChecked(intermediateB);
+                        advanced.setChecked(advancedB);
+                    }
                     Controller.setRange(1);
                 }
 
@@ -54,9 +59,14 @@ public class SettingsActivity extends AppCompatActivity {
                                          boolean isChecked) {
 
                 if (isChecked) {
-                    basic.setChecked(false);
-                    intermediate.setChecked(true);
-                    advanced.setChecked(false);
+                    intermediateB=true;
+                    if(intermediateB==true) {
+                        basicB=false;
+                        advancedB=false;
+                        basic.setChecked(basicB);
+                        intermediate.setChecked(intermediateB);
+                        advanced.setChecked(advancedB);
+                    }
                     Controller.setRange(2);
                 }
 
@@ -69,9 +79,14 @@ public class SettingsActivity extends AppCompatActivity {
                                          boolean isChecked) {
 
                 if (isChecked) {
-                    basic.setChecked(false);
-                    intermediate.setChecked(false);
-                    advanced.setChecked(true);
+                    advancedB=true;
+                    if(advancedB==true) {
+                        intermediateB=false;
+                        basicB=false;
+                        basic.setChecked(basicB);
+                        intermediate.setChecked(intermediateB);
+                        advanced.setChecked(advancedB);
+                    }
                     Controller.setRange(3);
                 }
 
