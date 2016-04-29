d832e7ec430bd72bd86874c61becb873320f6460
MeganLiang <meganliang15@hotmail.com>
2016-04-30 00:53:29 +1200

    "main menu"

--- app/src/main/java/com/welovekids/mathforkids/MainActivity.java
+++ app/src/main/java/com/welovekids/mathforkids/MainActivity.java
@@ -2,12 +2,59 @@ package com.welovekids.mathforkids;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        tv = (TextView) findViewById(R.id.start);
+        Button start = (Button) findViewById(R.id.start);
+
+        start.setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button acheive = (Button) findViewById(R.id.acheive);
+        acheive.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button settings = (Button) findViewById(R.id.settings);
+        settings.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+
 }
