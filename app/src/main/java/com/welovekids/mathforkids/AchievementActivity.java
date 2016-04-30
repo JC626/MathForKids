3b77b270ffced69086c28ff5f341caed0720b540
MeganLiang <meganliang15@hotmail.com>
2016-04-30 23:50:54 +1200

    "well, i needed to pull"

--- app/src/main/java/com/welovekids/mathforkids/AchievementActivity.java
+++ app/src/main/java/com/welovekids/mathforkids/AchievementActivity.java
@@ -27,57 +27,57 @@ public class AchievementActivity extends AppCompatActivity {
 
         if (Controller.getHighScore() >= 1) {
             //yellowtrophy for one Q ans
-            trophy1.setImageResource(R.drawable.yellowtrophy);
+            trophy1.setImageResource(R.drawable.yellow_trophy);
         }else {
-            trophy1.setImageResource(R.drawable.graytrophy);
+            trophy1.setImageResource(R.drawable.gray_trophy);
         }
 
 
         if (Controller.getHighScore() >= 5) {
             //yellowtrophy for 5 Q ans
-            trophy2.setImageResource(R.drawable.yellowtrophy);
+            trophy2.setImageResource(R.drawable.yellow_trophy);
 
         }else {
-            trophy2.setImageResource(R.drawable.graytrophy);
+            trophy2.setImageResource(R.drawable.gray_trophy);
         }
         if (Controller.getHighScore() >= 10) {
             //yellowtrophy for 10 Q ans
-            trophy3.setImageResource(R.drawable.yellowtrophy);
+            trophy3.setImageResource(R.drawable.yellow_trophy);
         }else {
-            trophy3.setImageResource(R.drawable.graytrophy);
+            trophy3.setImageResource(R.drawable.gray_trophy);
         }
 
 
         if (Controller.getHighScore() >= 20) {
             //yellowtrophy for 20 Q ans
-            trophy4.setImageResource(R.drawable.yellowtrophy);
+            trophy4.setImageResource(R.drawable.yellow_trophy);
         }else {
-            trophy4.setImageResource(R.drawable.graytrophy);
+            trophy4.setImageResource(R.drawable.gray_trophy);
         }
         if (Controller.getHighScore() >= 50) {
             //yellowtrophy for 50 Q ans
-            trophy5.setImageResource(R.drawable.yellowtrophy);
+            trophy5.setImageResource(R.drawable.yellow_trophy);
         }else {
-            trophy5.setImageResource(R.drawable.graytrophy);
+            trophy5.setImageResource(R.drawable.gray_trophy);
         }
         if (Controller.getHighScore() >= 100) {
             //yellowtrophy for 100 Q ans
-            trophy6.setImageResource(R.drawable.yellowtrophy);
+            trophy6.setImageResource(R.drawable.yellow_trophy);
         }else {
-            trophy6.setImageResource(R.drawable.graytrophy);
+            trophy6.setImageResource(R.drawable.gray_trophy);
         }
 
         if (Integer.parseInt(Controller.getCorrect()) >= 50) {
             //yellowtrophy for 100 Q ans
-            trophy7.setImageResource(R.drawable.yellowtrophy);
+            trophy7.setImageResource(R.drawable.yellow_trophy);
         }else {
-            trophy7.setImageResource(R.drawable.graytrophy);
+            trophy7.setImageResource(R.drawable.gray_trophy);
         }
         if (Integer.parseInt(Controller.getCorrect()) >= 100) {
             //yellowtrophy for 100 Q ans
-            trophy8.setImageResource(R.drawable.yellowtrophy);
+            trophy8.setImageResource(R.drawable.yellow_trophy);
         }else {
-            trophy8.setImageResource(R.drawable.graytrophy);
+            trophy8.setImageResource(R.drawable.gray_trophy);
         }
     }
 }
