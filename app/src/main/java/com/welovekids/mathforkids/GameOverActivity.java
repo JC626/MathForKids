d20cc96d73878d2d022ad266212477d981665e97
MeganLiang <meganliang15@hotmail.com>
2016-05-01 01:42:51 +1200

    check highscore

--- app/src/main/java/com/welovekids/mathforkids/GameOverActivity.java
+++ app/src/main/java/com/welovekids/mathforkids/GameOverActivity.java
@@ -12,6 +12,19 @@ public class GameOverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+        int correct = Integer.parseInt(Controller.getCorrect());
+        int wrong = Integer.parseInt(Controller.getTotalIncorrect());
+        int total = Controller.getTotalQuestions();
+        int highscore = Controller.getHighScore();
+
+        TextView totalCorrect = (TextView) findViewById(R.id.totalCorrect);
+        totalCorrect.append(String.valueOf(correct));
+//        TextView totalCorrect = (TextView) findViewById(R.id.totalCorrect);
+//        totalCorrect.append(String.valueOf(correct));
+//        TextView totalCorrect = (TextView) findViewById(R.id.totalCorrect);
+//        totalCorrect.append(String.valueOf(correct));
+//        TextView totalCorrect = (TextView) findViewById(R.id.totalCorrect);
+//        totalCorrect.append(String.valueOf(correct));
         //Get gameCorrect, gameQuestions, gameWrong, highSocre
         //Set to textviews
         //TextView highScore = (TextView)findViewById(R.id.roundScore);
