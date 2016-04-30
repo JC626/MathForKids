8a9f5eb642d3e37df5754a65fe5bc908748f1726
JC626 <janice.chin626@gmail.com>
2016-04-30 17:29:39 +1200

    Added Score

---
+++ app/src/main/java/com/welovekids/mathforkids/Score.java
@@ -0,0 +1,17 @@
+package com.welovekids.mathforkids;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Score extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        TextView totalCorrect = (TextView)findViewById(R.id.totalCorrect);
+        TextView totalWrong = (TextView)findViewById(R.id.totalWrong);
+        TextView totalQuestions = (TextView)findViewById(R.id.totalQuestions);
+}
